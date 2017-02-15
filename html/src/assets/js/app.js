eca41381763954c1497d761f1c6f39dacc8497e2
Dom Bonanni <dombonanni@gmail.com>
2017-02-15 15:14:59 -0500

    Continued.

--- html/src/assets/js/app.js
+++ html/src/assets/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   $('.table-of-contents').toc({
     content: '.content',
-    headings: 'h2,h3,h4'
+    headings: 'h2,h3'
   });
 
   $('.content h1, .content h2, .content h3, .content h4, .content h5, .content h6').each( function () {
