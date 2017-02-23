d23a1b7734312e45c424e57efd790c06cc60faff
Dom Bonanni <dombonanni@gmail.com>
2017-02-22 21:40:36 -0500

    Added timer partial.

--- html/src/assets/js/app.js
+++ html/src/assets/js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     headings: 'h2,h3'
   });
 
-  $('.content h1, .content h2, .content h3, .content h4, .content h5, .content h6').each( function () {
+  $('.content h1, .content h2, .content h3, .content h4, .content h5, .content h6').each(function() {
     var id = $(this).attr('id');
     $(this).parent().attr('data-magellan-target', id);
   });
@@ -16,4 +16,42 @@ $(document).ready(function() {
   $('.accordion h4').click(function() {
     $(this).siblings().slideToggle();
   });
+
+  //http://git.wimbarelds.nl/TimeCircles
+  $('.timer').TimeCircles({
+    start: false,
+    count_past_zero: false,
+    animation: 'smooth', // smooth or ticks.
+    circle_bg_color: '#e0f1ff',
+    fg_width: 0.1,
+    bg_width: 0.8,
+    time: {
+      Days: {
+        show: false,
+        color: '#428bca'
+      },
+      Hours: {
+        show: false,
+        color: '#428bca'
+      },
+      Minutes: {
+        show: true,
+        color: '#428bca'
+      },
+      Seconds: {
+        show: true,
+        color: '#428bca'
+      }
+    }
+  }).restart().stop();
+  $('.timer-start').click(function() {
+    $('.timer').TimeCircles().start();
+  });
+  $('.timer-stop').click(function() {
+    $('.timer').TimeCircles().stop();
+  });
+  $('.timer-restart').click(function() {
+    $('.timer').TimeCircles().restart().stop();
+  });
 });
+
