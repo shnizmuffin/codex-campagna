e215c3b3f4ec4da9e613f51d1a76d92a38bb7d51
Dom Bonanni <dombonanni@gmail.com>
2017-03-26 14:59:39 -0400

    Don't need to fire `.foundation()` more than once, right? Wait for the page to completely render before setting up all the handlers and listeners.

--- html/src/assets/js/app.js
+++ html/src/assets/js/app.js
@@ -1,4 +1,3 @@
-$(document).foundation();
 $(document).ready(function() {
 
   $('.table-of-contents').toc({
@@ -9,7 +8,22 @@ $(document).ready(function() {
   $('.content h1, .content h2, .content h3, .content h4, .content h5, .content h6').each(function() {
     var id = $(this).attr('id');
     $(this).parent().attr('data-magellan-target', id);
+    $(this).parent().attr('id', 'section_' + id);
   });
+
+  $('.content > section').each(function() {
+    var section_id = $(this).attr('data-magellan-target');
+    var section_offset = $(this).position().top;
+    var sticky_template = '<div class="stickies" data-sticky-container><div id="nav_' + section_id + '" class="sticky" data-options="marginTop:5;" style="width:100%" data-sticky-on="medium" data-sticky data-top-anchor="section_' + section_id + ':top" data-btm-anchor="section_' + section_id + ':bottom"><p>' + section_id + '</p></div></div>';
+    $(sticky_template)
+      .appendTo('.index')
+      // .offset({
+      //   top: section_offset
+      // })
+      // .addClass('offset-'+section_offset);
+  });
+
+  $('.stickies').foundation();
 
   $('.table-of-contents a').attr('data-close', '');
 
@@ -53,5 +67,5 @@ $(document).ready(function() {
   $('.timer-restart').click(function() {
     $('.timer').TimeCircles().restart().stop();
   });
-});
+}).foundation();
 
